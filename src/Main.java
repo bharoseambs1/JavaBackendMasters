f0a64c2f6f2eaf87ca419eea294b2965797cfd76
pankaj <pankaj.wagh43@gmail.com>
2025-09-03 21:24:06 +0530

    Pankaj first push

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Satish!");
         System.out.println("Hello Gaurav!");
+        System.out.println("Hello Pankaj!");
     }
 }
