54a4bb7f5e0baf5385c1d3f5edce33b6007ae50c
Gaurav <gauravjadhav96@hotmail.com>
2025-09-04 21:53:06 +0530

    Static Method util assignment 04-09-2025

---
+++ src/com/gaurav/util/MathUtil.java
@@ -0,0 +1,14 @@
+package com.gaurav.util;
+
+public class MathUtil {
+    public static int square(int a){
+        return a * a;
+    }
+
+    public static void main(String[] args) {
+        int output = MathUtil.square(2);
+        System.out.println(output);
+    }
+
+
+}
