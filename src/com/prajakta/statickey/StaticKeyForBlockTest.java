a1c2979f1581120784097cee1acbd1ac918a36e8
Admn <prajktapatil434@gmail.com>
2025-09-06 19:06:01 +0530

    Static keyword for block

---
+++ src/com/prajakta/statickey/StaticKeyForBlockTest.java
@@ -0,0 +1,33 @@
+package com.prajakta.statickey;
+
+public class StaticKeyForBlockTest {
+    public static void main(String[] args) {
+        System.out.println("message from main method");
+    }
+
+    static {
+        System.out.println("message from static block - 1");
+        System.out.println("to initiate/instantiate/initialization or invoke of static variable and methods");
+        System.out.println("static block execute before main method and if we have multiple block it will execute by top to bottom sequence");
+    }
+
+    static {
+        System.out.println("message from static block - 2");
+    }
+    static {
+        System.out.println("message from static block - 3");
+    }
+    static {
+        System.out.println("message from static block - 4");
+    }
+    static {
+        System.out.println("message from static block - 5");
+    }
+
+    //non-static block
+    /*{
+        System.out.println("message from non static block - 1");
+    }*/
+}
+
+
