d61636a72a21f3f188f0a6133f36c03b755ca47f
Sneha <snehasalunke108@gmail,com>
2025-09-12 12:15:31 +0530

    inheritance assignments

---
+++ src/com/sneha/inheritance/Person.java
@@ -0,0 +1,11 @@
+package com.sneha.inheritance;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
