228f704a3872cc25624dd1df6c66f556634c7d1c
Satish Bharose <satishdbharose@gmail.com>
2025-09-15 19:58:12 +0530

    oops program for my:satish branch

---
+++ src/com/satish/oopsconcept/encapsulaas/EncasulationTest.java
@@ -0,0 +1,39 @@
+package com.satish.oopsconcept.encapsulaas;
+
+import com.gaurav.opps.polymorphism.Bank;
+
+public class EncasulationTest {
+    public static void main(String[] args) {
+        //Assignment 1
+        /*Student stdObj1 = new Student();
+        stdObj1.setAge(10);
+        stdObj1.setName("Satish");
+        System.out.println(stdObj1.getAge() + " name: " + stdObj1.getName());
+
+        Student stdObj2 = new Student();
+        stdObj2.setAge(-10);
+        stdObj2.setName("Satish");
+
+        System.out.println(stdObj2.getAge() + " name: " + stdObj2.getName());*/
+
+        //Assignment 2
+        /*BankAccount bankObj = new BankAccount();
+        bankObj.deposit(1234567890, 1000);
+        bankObj.withdraw(1900);
+        System.out.println();*/
+
+        //Assignment 3
+        /*Circle circleObj = new Circle(5);
+        System.out.println(circleObj.calculateArea());*/
+
+        //Assignment 4
+        //Employee employeeObj = new Employee(101, "Satish", 10000);
+
+        //Assignment 5
+        /*Book bookObj = new Book("The Pash", "Ken James", 2025);
+        bookObj.setPublicationYear(2026);
+        System.out.println(bookObj.getAuthor());
+        System.out.println(bookObj.getTitle());
+        System.out.println(bookObj.getPublicationYear());*/
+    }
+}
