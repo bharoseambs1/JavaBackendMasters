650eeedcefac91fdfb24ff2594f6eff273e22561
Admn <prajktapatil434@gmail.com>
2025-09-15 13:18:46 +0530

    Abstraction and Encapsulation Practice

---
+++ src/com/prajakta/abstraction/Animal.java
@@ -0,0 +1,11 @@
+package com.prajakta.abstraction;
+
+public abstract class Animal {
+
+    public void displayAnimalSound(){
+    System.out.println(" Animal Makes Sound" );
+    }
+
+   public  abstract String makesound();
+}
+
