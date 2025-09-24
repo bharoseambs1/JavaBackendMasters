d450dd7910c5728f89481a0c35bce565072efcb8
Satish Bharose <satishdbharose@gmail.com>
2025-09-24 21:06:16 +0530

    Collection common class

--- src/com/common/CollectionCommonTest.java
+++ src/com/common/CollectionCommonTest.java
@@ -1,7 +1,18 @@
 package com.common;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.TreeSet;
+
 public class CollectionCommonTest {
     public static void main(String[] args) {
+
+        //Satish - code starts here
+        HashSet<String> set = new HashSet<>(Arrays.asList("Apple","Banana","Apple","Mango"));
+        TreeSet<String> sorted = new TreeSet<>(set); // sorting
+        System.out.println("Sorted Set: " + sorted);
+        System.out.println("Count: " + set.size());
+        //Satish - code ends here
 
     }
 }
