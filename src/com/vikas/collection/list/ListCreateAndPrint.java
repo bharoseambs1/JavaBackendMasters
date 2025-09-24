b9e9ff4444b57ddeb4ee8c2fd7031b02c98e65e1
vikas <hvikas688@gmail.com>
2025-09-24 17:43:09 +0530

    List and Set Assignment

---
+++ src/com/vikas/collection/list/ListCreateAndPrint.java
@@ -0,0 +1,21 @@
+package com.vikas.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListCreateAndPrint {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Orange");
+        fruits.add("Grapes");
+        System.out.println("Fruits List:");
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
