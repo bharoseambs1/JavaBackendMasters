cd5ab567784cfe95a1cac09c9f7687b164ad618a
Sneha <snehasalunke108@gmail,com>
2025-09-25 16:42:43 +0530

    Common test class pushed

---
+++ src/com/common/CollectionCommonTest.java
@@ -0,0 +1,20 @@
+package com.common;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class CollectionCommonTest {
+    public static void main(String[] args) {
+        //Sneha - code starts here
+        HashSet<String> set = new HashSet<>(Arrays.asList("Apple","Banana","Apple","Mango"));
+        TreeSet<String> sorted = new TreeSet<>(set); // sorting
+        System.out.println("Sorted Set: " + sorted);
+        System.out.println("Count: " + set.size());
+        //YourName- code ends here
+    }
+}
+
+
+
+
