6116ee4aec5125dba238446151cec4d75f059cfa
Gaurav <gauravjadhav96@hotmail.com>
2025-09-25 19:40:17 +0530

    CollectionCommonTest class pushed

--- src/com/common/CollectionCommonTest.java
+++ src/com/common/CollectionCommonTest.java
@@ -7,12 +7,12 @@ import java.util.TreeSet;
 public class CollectionCommonTest {
     public static void main(String[] args) {
 
-        //Satish - code starts here
+        //Gaurav - code starts here
         HashSet<String> set = new HashSet<>(Arrays.asList("Apple","Banana","Apple","Mango"));
         TreeSet<String> sorted = new TreeSet<>(set); // sorting
         System.out.println("Sorted Set: " + sorted);
         System.out.println("Count: " + set.size());
-        //Satish - code ends here
+        //Gaurav - code ends here
 
     }
 }
