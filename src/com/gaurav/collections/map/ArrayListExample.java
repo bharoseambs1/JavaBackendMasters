225b1eb0c525a5bf3ecc23b3715de8882a6f7007
Gaurav <gauravjadhav96@hotmail.com>
2025-09-30 18:13:22 +0530

    map and hashmapvsconcurrenthashmap pushed under gaurav-main branch

---
+++ src/com/gaurav/collections/map/ArrayListExample.java
@@ -0,0 +1,19 @@
+package com.gaurav.collections.map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+        list.add("Java"); // duplicate allowed
+        list.add(null);
+        System.out.println("ArrayList: " + list);
+
+        //default size of arraylist is 10
+        //it increases by 50% when it reaches its capacity
+    }
+}
---
+++ src/com/gaurav/collections/map/LinkedListExample.java
@@ -0,0 +1,16 @@
+package com.gaurav.collections.map;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {  //doubly linked list it introduced in jdk 1.2
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+        list.add("Java"); // duplicate allowed
+        list.add(null);
+        System.out.println("LinkedList: " + list);
+    }
+}
