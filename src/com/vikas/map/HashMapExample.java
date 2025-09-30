a2dde1cca40cc4b7f0f8fa80e948fe2eb3163771
vikas <hvikas688@gmail.com>
2025-09-30 20:02:21 +0530

    Map Collection class code edit with my branch

---
+++ src/com/vikas/map/HashMapExample.java
@@ -0,0 +1,17 @@
+package com.vikas.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Apple");
+        map.put(2, "Banana");
+        map.put(3, "Mango");
+        System.out.println("Value for key 2: " + map.get(2));
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+}
---
+++ src/com/vikas/map/LinkedHashMapExample.java
@@ -0,0 +1,14 @@
+package com.vikas.map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapExample {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("A", 100);
+        map.put("B", 200);
+        map.put("C", 300);
+        System.out.println(map); // Output: {A=100, B=200, C=300}
+    }
+}
