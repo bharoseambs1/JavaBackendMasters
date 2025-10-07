962b6821c417d2326e6b31d839cfd632da2030ae
prajktapatil434 <prajktapatil434@gmail.com>
2025-10-07 20:05:00 +0530

    Prajakta p main (#33)
    * map collection examples
    
    * Stack,Vector,Treeset examples
    
    ---------
    
    Co-authored-by: Satish Bharose <satishdbharose@gmail.com>

---
+++ src/com/prajakta/collection/map/StackDemo.java
@@ -0,0 +1,27 @@
+package com.prajakta.collection.map;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        Iterable itr = new Stack();
+        Collection colln= new Stack();
+        List list = new Stack();
+        Vector vector = new Stack();
+        Stack stack = new Stack();
+
+
+        //FIFO  -- First In First Out  (Queue)
+        //LIFO  -- Last In First Out   (Stack)
+
+
+        Iterable iterableV = new Vector();
+        Collection collectionV = new Vector();
+        List listV = new Vector();
+        Vector vectorV = new Vector();
+
+    }
+}
---
+++ src/com/prajakta/collection/map/Student.java
@@ -0,0 +1,13 @@
+package com.prajakta.collection.map;
+
+public class Student {
+    int id;
+    String name;
+    int age;
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
